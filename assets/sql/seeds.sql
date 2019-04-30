95933869e02fdc69d9ac735804d1a58829d00b9b
barkgnar <45748185+kenzrad@users.noreply.github.com>
2019-04-30 12:35:47 -0400

    database table added

---
+++ assets/sql/seeds.sql
@@ -0,0 +1,17 @@
+/* Seeds for SQL table. We haven't discussed this type of file yet */
+USE bamazonDB;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES 
+    ("Locking carabinders", "Climbing", 18.50, 30), 
+    ("ATC", "Climbing", 25.00, 21), 
+    ("grigri", "Climbing", 99.50, 10), 
+    ("Quickdraws 5-Pack", "Climbing", 65.00, 30), 
+    ("Leader 3-Pack", "Fly Flishing", 15.99, 25), 
+    ("Minibeads", "Fly Fishing", 5.99, 10), 
+    ("Lightweight Reel", "Fly Fishing", 125.50, 6), 
+    ("Adams Fly Assorted 10-Pack", "Fly Fishing", 20.00, 15),
+    ("Wool midweight socks", "Apparel", 9.99, 25), 
+    ("Silk undershirt", "Apparel", 32.00, 10), 
+    ("Cargo vest", "Apparel", 45.50, 8), 
+    ("Down vest", "Apparel", 75.00, 10),
