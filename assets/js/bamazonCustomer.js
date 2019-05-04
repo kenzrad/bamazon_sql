e3de688d4777ba41d51d74e87caca6f495758291
barkgnar <45748185+kenzrad@users.noreply.github.com>
2019-05-04 00:02:05 -0400

    polished up

--- assets/js/bamazonCustomer.js
+++ assets/js/bamazonCustomer.js
@@ -15,27 +15,45 @@ connection.connect(function(err) {
     productOverview();
   });
 
-// Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
+// Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale
 function productOverview() {
+  console.log("");
+  console.log("");
+  console.log(chalk.blue("--------------------------------------------------------"));
+  console.log("");
+  console.log(`
+    ${chalk.cyan.bold("WELCOME TO BAMAZON!")}
+     
+    ${chalk.reset.italic.magenta("A CLI Outdoor Gear & Apparel shopping app!")}
+  `);
+  
+  console.log("");
+  console.log(chalk.blue("---------------------------------------------------------"));
+  console.log("");
+  console.log("");
     inquirer
     .prompt({
       name: "welcomePrompt",
       type: "list",
-      message: `
-        ${chalk.cyan.bold("WELCOME TO BAMAZON!")}
-        ${chalk.reset.italic.magenta("A CLI Outdoor Gear & Apparel shopping app!")}
-          
-        ${chalk.cyan.green("Would you like to see our list of products?")}
+      message: `${chalk.cyan.green("Would you like to see our list of products?")}
+        
         `,
       choices: ["YES PLEASE!", "No thank you!"]
     })
     .then(function(answer) {
-      console.log(answer);
+      console.log("");
+      console.log("");
       if (answer.welcomePrompt === "YES PLEASE!") {
         listItems();
       }
       else {
+        console.log("");
+        console.log(chalk.blue("---------------------------------------------------------"));
+        console.log("");
         console.log(`${chalk.cyan("That's okay, come see us again when you want to shop for some awesome outdoor gear and apparel!")}`)
+        console.log("");
+        console.log(chalk.blue("---------------------------------------------------------"));
+        console.log("");
         connection.end();
       }
 
@@ -43,14 +61,15 @@ function productOverview() {
 }
 
 function listItems() {
-  var query = "SELECT item_id, product_name, price FROM products WHERE stock_quantity > 0";
+  var query = "SELECT item_id, product_name, price, stock_quantity FROM products WHERE stock_quantity > 0";
   connection.query(query, function(err, res) {
-    console.log("Calculating....")
+    console.log(`${chalk.magenta("Items in stock:")}`)
     for (var i = 0; i < res.length; i++) {
       console.log(`
         Product ID:  ${chalk.yellow(res[i].item_id)}
         Item:        ${chalk.cyan(res[i].product_name)}
         Price:       ${chalk.cyan("$")}${chalk.cyan(res[i].price.toFixed(2))}
+        In Stock:    ${chalk.cyan(res[i].stock_quantity)}
         `
       );
     }
@@ -58,17 +77,20 @@ function listItems() {
     .prompt({
       name: "readyToBuy",
       type: "list",
-      message: "Scroll up to see what we have to offer! Once you are ready to purchase, select 'okay'!",
+      message: `${chalk.green("Scroll up to see what we have to offer! Once you are ready to purchase select")}${chalk.magenta.bold("'okay'")}
+       
+       `,
       choices: ["okay", "I've changed my mind!"]
     })
     .then(function(answer) {
-      console.log(answer.readyToBuy);
       if(answer.readyToBuy === "okay") {
-        console.log("okay!!");
+        console.log("");
+        console.log("");
         purchasePrompt();
       }
       else {
-        console.log(`${chalk.cyan("That's okay, come see us again when you want to shop for some awesome outdoor gear and apparel!")}`)
+        console.log(`
+          ${chalk.magenta("That's okay, come see us again when you want to shop for some awesome outdoor gear and apparel!")}`)
         connection.end();
       }
     });
@@ -81,49 +103,54 @@ function purchasePrompt() {
     {
       name: "productID",
       type: "number",
-      message: "Please enter the Product ID for the item you would like to purchase",
+      message: `${chalk.green("Please enter the Product ID for the item you would like to purchase")}`,
       validate: function(value) {
         if (isNaN(value) === false) {
           return true;
         }
-        console.log("That's not a number!!");
+        console.log(`${chalk.red("That's not a number!!")}`);
         return false;
       }
     },
     {
       name: "amount",
       type: "number",
-      message: "How many do you want to purchase?",
+      message: `${chalk.green("How many do you want to purchase?")}`,
       validate: function(value) {
         if (isNaN(value) === false) {
           return true;
         }
-        console.log("That's not a number!!");
+        console.log(`${chalk.red("That's not a number!!")}`);
         return false;
       }
     }
   ])
   .then(function(answer) {
+    console.log("");
+    console.log("");
     if (answer.itemQuantity === "0") {
       productOverview();
     }
     else {
-      console.log("OR ELSE!!!");
-      //Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+      //check inventory
       var query = "SELECT item_id, product_name, price, stock_quantity FROM products WHERE ?";
       connection.query(query,{ item_id: answer.productID }, function(err, res) {
-      //However, if your store does have enough of the product, you should fulfill the customer's order & update SQL database to refelect the remaining quantity
-        if (res[0].stock_quantity > 0) {
-          console.log("Processing your order....");
+        //fulfill order and run stock update function
+        if (res[0].stock_quantity > answer.amount) {
           var productID = answer.productID;
           var newQuantity = parseInt(res[0].stock_quantity - answer.amount);
           updateStock(productID, newQuantity, answer.amount);
-          //Once the order goes through, show the customer the total cost of their purchase
         }
-        //If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
+        //insuffucient inventory
         else {
-          console.log("I'm sorry! We are all out of that item. Please come again soon!!")
-          connection.end();
+          console.log(`
+            ${chalk.red("OH NO!!!!")}
+             
+            ${chalk.magenta("We don't have enough in stock of that items.")}
+            
+            ${chalk.green("Please try again...")}
+          `);
+          purchasePrompt();
         }
       });
     }
@@ -142,10 +169,17 @@ function calculatePrice(id, quantity) {
   var query = "SELECT product_name, price FROM products WHERE ?";
   connection.query(query, { item_id: id }, function(err, res) {
     var totalPrice = parseInt(res[0].price * quantity);
-    console.log(totalPrice);
+    console.log(`
+      ${chalk.cyan("You total comes to")}${chalk.bold.yellow(" $")}${chalk.bold.yellow(totalPrice.toFixed(2))}
+        
+      ${chalk.cyan("You're order will be placed once you figure out how to pay me!")}
+  
+      ${chalk.green("Thank you for trying to shop at BAMAZON!")}
+    `);
   });
   connection.end();
 }
+
 
 
 
